7541a0a9ef192815d2a0c8fb793c511add45e713
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-04-25 20:34:12 +0900

    looping

--- src/automate_GA.py
+++ src/automate_GA.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 from execute_GA import GeneticAlgorithmAligner as GA1
 from execute_GAv2 import GeneticAlgorithmAligner as GA2
 
@@ -15,5 +18,16 @@ param_table = [
     {"pdb1":"3c9a.pbd", "pdb2":"1jl9.pdb",  "aln":"seq.aln.fasta", "pop":500, "gen":100, "rec":0.7, "mut":0.7}
 ]
 
-for pram in param_table:
-    ...
+for param in param_table:
+    for i in range(5):
+        ga1=GA1(
+            param["pdb1"], param["pdb2"], param["aln"], param["pop"], param["gen"], param["mut"], param["rec"], i, False
+        )
+        population = ga1.prepare_ga()
+    
+        for i in range(ga1.gen_num):
+            mutated = ga1.mutation(population)
+            recombinated = ga1.recombination(mutated)
+            fitness_vals = np.array([ga1.calc_fitness(ind) for ind in recombinated])
+            population = ga1.selection(recombinated, fitness_vals, i)
+        ga1
